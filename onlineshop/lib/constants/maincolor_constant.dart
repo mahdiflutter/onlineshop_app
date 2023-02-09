d91cbdd3e9b2427b45e7868bdcddeb20fa155125
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-02-09 15:06:30 +0330

    Create maincolor_constant.dart

---
+++ onlineshop/lib/constants/maincolor_constant.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MainColors{
+  static const mainBlue=Color(0xff3B5EDF);
+  static const mainGreen=Color(0xff1DB68B);
+  static const mainRed=Color(0xffD02026);
+  static const mainGray=Color(0xff858585);
+  static const mainWhite=Color(0xffeeeeee);
+  static const mainBackGround=Color(0xffE5E5E5);
+  
+}
