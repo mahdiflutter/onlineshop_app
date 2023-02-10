667db6f8e85f3ddc630754992a5c8f44d92c3a20
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-02-10 14:54:41 +0330

    Update profile_screen.dart

--- onlineshop/lib/screens/profile_screen.dart
+++ onlineshop/lib/screens/profile_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:onlineshop/constants/maincolor_constant.dart';
 import 'package:onlineshop/widgets/categoryitem_widget.dart';
+
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
@@ -17,10 +18,11 @@ class _ProfileScreenState extends State<ProfileScreen> {
         child: Column(
           children: [
             const CustomAppbar(),
-            const SizedBox(height: 20,),
+            const SizedBox(
+              height: 20,
+            ),
             Wrap(
               spacing: 25,
-
               children: const [
                 ProfileItem(),
                 ProfileItem(),
@@ -32,39 +34,43 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 ProfileItem(),
                 ProfileItem(),
                 ProfileItem(),
-
               ],
             ),
             const Spacer(),
-            const Text('اپل شاپ',
-            style: TextStyle(
-              fontFamily: 'SM',
-              color: MainColors.mainGray,
-              fontSize: 14,
+            const Text(
+              'اپل شاپ',
+              style: TextStyle(
+                fontFamily: 'SM',
+                color: MainColors.mainGray,
+                fontSize: 14,
+              ),
             ),
+            const Text(
+              'Version 1.0.0',
+              style: TextStyle(
+                fontFamily: 'SM',
+                color: MainColors.mainGray,
+                fontSize: 14,
+              ),
             ),
-            const Text('Version 1.0.0',
-            style: TextStyle(
-              fontFamily: 'SM',
-              color: MainColors.mainGray,
-              fontSize: 14,
+            const Text(
+              'GitHub:mahdiflutter',
+              style: TextStyle(
+                fontFamily: 'SM',
+                color: MainColors.mainGray,
+                fontSize: 14,
+              ),
             ),
-            ),
-            const Text('GitHub:mahdiflutter',
-            style: TextStyle(
-              fontFamily: 'SM',
-              color: MainColors.mainGray,
-              fontSize: 14,
-            ),
-            ),
-            const SizedBox(height: 20,)
-
+            const SizedBox(
+              height: 20,
+            )
           ],
         ),
       ),
     );
   }
 }
+
 class CustomAppbar extends StatelessWidget {
   const CustomAppbar({
     super.key,
@@ -73,40 +79,43 @@ class CustomAppbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding:  const EdgeInsets.symmetric(vertical: 20,horizontal: 44),
-      child:Container(
-        decoration:  BoxDecoration(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 44),
+      child: Container(
+        decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(15),
-          ),
-          height: 46.0,
-          child: Row(
-            children: const [
-              SizedBox(width: 12,),
-              Icon(
-                Icons.apple,
-                color: MainColors.mainBlue,
-                size: 30,
-              ),
-              Spacer(),
-              Text('حساب کاربری',
+        ),
+        height: 46.0,
+        child: Row(
+          children: const [
+            SizedBox(
+              width: 12,
+            ),
+            Icon(
+              Icons.apple,
+              color: MainColors.mainBlue,
+              size: 30,
+            ),
+            Spacer(),
+            Text(
+              'حساب کاربری',
               style: TextStyle(
                 color: MainColors.mainBlue,
                 fontSize: 17,
-
-                
               ),
-              ),
-              Spacer(),
-              Icon(Icons.person_pin_rounded,
+            ),
+            Spacer(),
+            Icon(
+              Icons.person_pin_rounded,
               color: MainColors.mainBlue,
               size: 30,
-              ),
-              SizedBox(width: 15,),
-              
-            ],
-          ),
-      ) ,
+            ),
+            SizedBox(
+              width: 15,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -119,38 +128,40 @@ class ProfileItem extends StatelessWidget {
     return Column(
       children: [
         Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 5,vertical: 10),
-                    child: Container(
-                    width: 56.0,
-                    height: 56,
-                    
-                    decoration: ShapeDecoration(
-                      shadows:const [
-                        BoxShadow(
-                          blurRadius: 5,
-                          spreadRadius: -3,
-                          color: MainColors.mainRed,
-                          offset: Offset(0, 8),
-                        )
-                      ],
-                      color: MainColors.mainRed,
-                      shape: ContinuousRectangleBorder(
-                        borderRadius: BorderRadius.circular(40),
-                      )
-                    ),
-                    child:const Icon(Icons.settings,color:Colors.white,size: 35,),
-                    
-                    ),
-                  ),
-                  const SizedBox(height: 10,),
-                  const Text('تنظیمات',
-                    style: TextStyle(
-                      fontFamily: 'SB',
-                      fontSize: 12,
-                    ),
-                  
-                  ),
-
+          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+          child: Container(
+            width: 56.0,
+            height: 56,
+            decoration: ShapeDecoration(
+                shadows: const [
+                  BoxShadow(
+                    blurRadius: 5,
+                    spreadRadius: -3,
+                    color: MainColors.mainRed,
+                    offset: Offset(0, 8),
+                  )
+                ],
+                color: MainColors.mainRed,
+                shape: ContinuousRectangleBorder(
+                  borderRadius: BorderRadius.circular(40),
+                )),
+            child: const Icon(
+              Icons.settings,
+              color: Colors.white,
+              size: 35,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        const Text(
+          'تنظیمات',
+          style: TextStyle(
+            fontFamily: 'SB',
+            fontSize: 12,
+          ),
+        ),
       ],
     );
   }
