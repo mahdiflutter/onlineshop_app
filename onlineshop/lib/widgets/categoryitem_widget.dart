bcdef92ee313e101b4f271bb0d80150fd8b9755f
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-02-10 14:54:31 +0330

    Update categoryitem_widget.dart

--- onlineshop/lib/widgets/categoryitem_widget.dart
+++ onlineshop/lib/widgets/categoryitem_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:onlineshop/constants/maincolor_constant.dart';
+
 class CateGoryItem extends StatelessWidget {
   const CateGoryItem({super.key});
 
@@ -8,38 +9,40 @@ class CateGoryItem extends StatelessWidget {
     return Column(
       children: [
         Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 5),
-                    child: Container(
-                    width: 56.0,
-                    height: 56,
-                    
-                    decoration: ShapeDecoration(
-                      shadows:const [
-                        BoxShadow(
-                          blurRadius: 5,
-                          spreadRadius: -3,
-                          color: MainColors.mainBlue,
-                          offset: Offset(0, 8),
-                        )
-                      ],
-                      color: MainColors.mainBlue,
-                      shape: ContinuousRectangleBorder(
-                        borderRadius: BorderRadius.circular(40),
-                      )
-                    ),
-                    child:const Icon(Icons.phone_android,color:MainColors.mainWhite,size: 35,),
-                    
-                    ),
-                  ),
-                  const SizedBox(height: 10,),
-                  const Text('موبایل',
-                    style: TextStyle(
-                      fontFamily: 'SB',
-                      fontSize: 12,
-                    ),
-                  
-                  ),
-
+          padding: const EdgeInsets.symmetric(horizontal: 5),
+          child: Container(
+            width: 56.0,
+            height: 56,
+            decoration: ShapeDecoration(
+                shadows: const [
+                  BoxShadow(
+                    blurRadius: 5,
+                    spreadRadius: -3,
+                    color: MainColors.mainBlue,
+                    offset: Offset(0, 8),
+                  )
+                ],
+                color: MainColors.mainBlue,
+                shape: ContinuousRectangleBorder(
+                  borderRadius: BorderRadius.circular(40),
+                )),
+            child: const Icon(
+              Icons.phone_android,
+              color: MainColors.mainWhite,
+              size: 35,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        const Text(
+          'موبایل',
+          style: TextStyle(
+            fontFamily: 'SB',
+            fontSize: 12,
+          ),
+        ),
       ],
     );
   }
