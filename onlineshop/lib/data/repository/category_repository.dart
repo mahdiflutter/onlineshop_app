8b04d0fb5ed5b4db89ce44b2b80914010d175237
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-02-16 08:37:42 +0330

    Create category_repository.dart

---
+++ onlineshop/lib/data/repository/category_repository.dart
@@ -0,0 +1,25 @@
+import 'package:dartz/dartz.dart';
+import 'package:onlineshop/data/remotedatasource/category_datasource.dart';
+import 'package:onlineshop/di/di.dart';
+import 'package:onlineshop/utils/api_exception.dart';
+import 'package:onlineshop/data/model/category_model.dart';
+
+abstract class ICategoryRepository {
+  Future<Either<String, List<Category>>> getCategory();
+}
+
+class CategoryRepository implements ICategoryRepository {
+  final ICategoryDatasource _dataSource = locator.get();
+  @override
+  Future<Either<String, List<Category>>> getCategory() async {
+    try {
+      var response = await _dataSource.getCategory();
+      print(response);
+      return Right(response);
+    } on ApiException catch (ex) {
+      return Left(ex.errorMessage!);
+    } catch (ex) {
+      return const Left('not found error');
+    }
+  }
+}
