5541e5d46c685fadbf2f89574c27298c375a0c2f
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-02-16 08:38:04 +0330

    Update category_screen.dart

--- onlineshop/lib/screens/category_screen.dart
+++ onlineshop/lib/screens/category_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:onlineshop/constants/maincolor_constant.dart';
+import 'package:onlineshop/data/model/category_model.dart';
+import 'package:onlineshop/data/repository/category_repository.dart';
+import 'package:onlineshop/di/di.dart';
+import 'package:onlineshop/widgets/cachedimage_widget.dart';
 
 class CategoryScreen extends StatefulWidget {
   const CategoryScreen({super.key});
@@ -9,6 +13,7 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
+  List<Category> _categoryList = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,31 +21,61 @@ class _CategoryScreenState extends State<CategoryScreen> {
       body: SafeArea(
         child: CustomScrollView(
           slivers: [
+            SliverToBoxAdapter(
+              child: ElevatedButton(
+                onPressed: () async {
+                  final ICategoryRepository _repository = locator.get();
+                  var response = await _repository.getCategory();
+                  response.fold(
+                    (l) {
+                      print(l);
+                    },
+                    (r) {
+                      setState(() {
+                        _categoryList = r;
+                      });
+                      print(_categoryList);
+                    },
+                  );
+                },
+                child: const Text('get category'),
+              ),
+            ),
             const SliverToBoxAdapter(
               child: CustomAppbar(),
             ),
-            SliverPadding(
-              padding: const EdgeInsets.symmetric(horizontal: 44, vertical: 20),
-              sliver: SliverGrid(
-                delegate: SliverChildBuilderDelegate(
-                  (context, index) {
-                    return Container(
-                      decoration: BoxDecoration(
-                        color: MainColors.mainRed,
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                    );
-                  },
-                  childCount: 10,
-                ),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 20,
-                  mainAxisSpacing: 20,
-                ),
-              ),
+            CategoryList(
+              list: _categoryList,
             )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CategoryList extends StatelessWidget {
+  CategoryList({
+    super.key,
+    required this.list,
+  });
+  List<Category>? list;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverPadding(
+      padding: const EdgeInsets.symmetric(horizontal: 44, vertical: 20),
+      sliver: SliverGrid(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            return CachedImage(imageUrl: list![index].thumbnail);
+          },
+          childCount: list!.length,
+        ),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
         ),
       ),
     );
