9664147aa8fc4200ea305a6cd44c10637e5d38b6
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-02-16 14:49:20 +0330

    Create caregory_state.dart

---
+++ onlineshop/lib/bloc/category/caregory_state.dart
@@ -0,0 +1,11 @@
+import 'package:onlineshop/data/model/category_model.dart';
+
+abstract class CategoryState {}
+
+class CategoryLoadingState extends CategoryState {}
+class CategoryInitState extends CategoryState{}
+class CategoryErrorState extends CategoryState{}
+class CategoryResponseState extends CategoryState {
+  List<Category>? response;
+  CategoryResponseState(this.response);
+}
