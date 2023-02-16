5dbfd4451c6e440c6b34e79d9881cd338de29d73
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-02-16 14:49:41 +0330

    Create banners_repository.dart

---
+++ onlineshop/lib/data/repository/banners_repository.dart
@@ -0,0 +1,26 @@
+import 'package:dartz/dartz.dart';
+import 'package:onlineshop/data/model/banner_model.dart';
+import 'package:onlineshop/data/remotedatasource/banners_datasource.dart';
+import 'package:onlineshop/di/di.dart';
+import 'package:onlineshop/utils/api_exception.dart';
+
+abstract class IBannerRepositiry {
+  Future<Either<String, List<BannerAds>>> getBanners();
+}
+
+class BannerRepository implements IBannerRepositiry {
+  final IBannersDataSource _dataSource=locator.get();
+  @override
+  Future<Either<String, List<BannerAds>>> getBanners() async {
+    try {
+      var response=await _dataSource.getBanners();
+      return Right(response);
+    } on ApiException catch (ex) {
+      return Left(
+        '${ex.errorMessage}',
+      );
+    } catch (ex) {
+      return const Left('An unknown error has occurred');
+    }
+  }
+}
