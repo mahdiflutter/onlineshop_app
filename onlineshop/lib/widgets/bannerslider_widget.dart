f6b1401670e83e2bf7d7e0d3b6be4a1eb9b24d24
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-02-19 12:33:04 +0330

    Update bannerslider_widget.dart

--- onlineshop/lib/widgets/bannerslider_widget.dart
+++ onlineshop/lib/widgets/bannerslider_widget.dart
@@ -5,42 +5,45 @@ import 'package:onlineshop/widgets/cachedimage_widget.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class BannerSlider extends StatelessWidget {
-  List<BannerAds>? bannerList;
-   BannerSlider({super.key,this.bannerList});
+  List<BannerModel>? bannerList;
+  BannerSlider({super.key, this.bannerList});
 
   @override
   Widget build(BuildContext context) {
     final controller = PageController(viewportFraction: 0.9);
     return SizedBox(
       height: 170,
-      child: Stack(alignment: AlignmentDirectional.bottomCenter, children: [
-        PageView.builder(
-          controller: controller,
-          itemCount: bannerList!.length,
-          itemBuilder: (context, index) => Container(
-            margin: const EdgeInsets.symmetric(horizontal: 6),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              color: Colors.white,
-            ),
-            child: CachedImage(imageUrl:bannerList![index].thumbnail),
-          ),
-        ),
-        Positioned(
-          bottom: 10,
-          child: SmoothPageIndicator(
+      child: Stack(
+        alignment: AlignmentDirectional.bottomCenter,
+        children: [
+          PageView.builder(
             controller: controller,
-            count: 3,
-            effect: const ExpandingDotsEffect(
-              dotHeight: 10,
-              dotWidth: 10,
-              expansionFactor: 5,
-              activeDotColor: MainColors.mainBlue,
-              dotColor: Colors.white,
+            itemCount: bannerList!.length,
+            itemBuilder: (context, index) => Container(
+              margin: const EdgeInsets.symmetric(horizontal: 6),
+              decoration: const BoxDecoration(),
+              child: CachedImage(
+                imageUrl: bannerList![index].thumbnail,
+                radius: 15,
+              ),
             ),
           ),
-        )
-      ]),
+          Positioned(
+            bottom: 10,
+            child: SmoothPageIndicator(
+              controller: controller,
+              count: 3,
+              effect: const ExpandingDotsEffect(
+                dotHeight: 6,
+                dotWidth: 6,
+                expansionFactor: 3,
+                activeDotColor: MainColors.mainBlue,
+                dotColor: Colors.white,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
