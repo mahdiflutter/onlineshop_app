07438e5e77faccdcad44fdc1c0c5e0af02b0ca01
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-02-19 12:32:35 +0330

    Update home_state.dart

--- onlineshop/lib/bloc/home/home_state.dart
+++ onlineshop/lib/bloc/home/home_state.dart
@@ -1,4 +1,7 @@
+import 'package:dartz/dartz.dart';
 import 'package:onlineshop/data/model/banner_model.dart';
+import 'package:onlineshop/data/model/category_model.dart';
+import 'package:onlineshop/data/model/product_model.dart';
 
 abstract class HomeState {}
 
@@ -7,6 +10,16 @@ class HomeinitState extends HomeState {}
 class HomeLodingState extends HomeState {}
 
 class HomeResponseState extends HomeState {
-  List<BannerAds> response;
-  HomeResponseState(this.response);
+  Either<String, List<BannerModel>> responseBanners;
+  Either<String, List<CategoryModel>> responseCategory;
+  Either<String, List<ProductModel>> responseProducts;
+  Either<String, List<ProductModel>> responseBestSellersProducts;
+  Either<String, List<ProductModel>> responseHotestProducts;
+  HomeResponseState(
+    this.responseBanners,
+    this.responseCategory,
+    this.responseProducts,
+    this.responseBestSellersProducts,
+    this.responseHotestProducts,
+  );
 }
