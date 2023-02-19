6fa3e5cec3f258d4d4bcfe171edb3bcbb46424fa
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-02-19 12:32:33 +0330

    Update home_bloc.dart

--- onlineshop/lib/bloc/home/home_bloc.dart
+++ onlineshop/lib/bloc/home/home_bloc.dart
@@ -2,23 +2,38 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:onlineshop/bloc/home/home_event.dart';
 import 'package:onlineshop/bloc/home/home_state.dart';
 import 'package:onlineshop/data/repository/banners_repository.dart';
+import 'package:onlineshop/data/repository/category_repository.dart';
+import 'package:onlineshop/data/repository/products_repository.dart';
 import 'package:onlineshop/di/di.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   final IBannerRepositiry _bannerRepository = locator.get();
+  final ICategoryRepository _categoryRepository = locator.get();
+  final IProductsRepository _productsRepository = locator.get();
+
   HomeBloc() : super(HomeinitState()) {
     on<HomeSendRequestEvent>(
       (event, emit) async {
         emit(
           HomeLodingState(),
         );
+
         var bannerList = await _bannerRepository.getBanners();
-        bannerList.fold((l){
-          print(l);
-        }, (r){
-          emit(HomeResponseState(r));
-        });
-       
+        var categoryList = await _categoryRepository.getCategory();
+        var productsList = await _productsRepository.getAllProducts();
+        var bestSellersList =
+            await _productsRepository.getBestSellersProducts();
+        var hotestList = await _productsRepository.getHotestProducts();
+
+        emit(
+          HomeResponseState(
+            bannerList,
+            categoryList,
+            productsList,
+            bestSellersList,
+            hotestList,
+          ),
+        );
       },
     );
   }
