d8bcb808f4192c1de14e484fd3aa1ab7e4a03353
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-02-19 12:32:47 +0330

    Update banners_repository.dart

--- onlineshop/lib/data/repository/banners_repository.dart
+++ onlineshop/lib/data/repository/banners_repository.dart
@@ -5,13 +5,13 @@ import 'package:onlineshop/di/di.dart';
 import 'package:onlineshop/utils/api_exception.dart';
 
 abstract class IBannerRepositiry {
-  Future<Either<String, List<BannerAds>>> getBanners();
+  Future<Either<String, List<BannerModel>>> getBanners();
 }
 
 class BannerRepository implements IBannerRepositiry {
   final IBannersDataSource _dataSource=locator.get();
   @override
-  Future<Either<String, List<BannerAds>>> getBanners() async {
+  Future<Either<String, List<BannerModel>>> getBanners() async {
     try {
       var response=await _dataSource.getBanners();
       return Right(response);
