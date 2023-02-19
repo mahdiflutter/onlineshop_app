7007182d4c5655fbfec25946da1bc16864de343d
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-02-19 12:33:06 +0330

    Update cachedimage_widget.dart

--- onlineshop/lib/widgets/cachedimage_widget.dart
+++ onlineshop/lib/widgets/cachedimage_widget.dart
@@ -3,15 +3,21 @@ import 'package:flutter/material.dart';
 
 class CachedImage extends StatelessWidget {
   String? imageUrl;
+  double? radius;
   CachedImage({
     super.key,
     required this.imageUrl,
+    this.radius = 0,
   });
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
+      borderRadius: BorderRadius.all(
+        Radius.circular(radius!),
+      ),
       child: CachedNetworkImage(
+      
         errorWidget: (context, url, error) => Container(
           color: Colors.red[200],
         ),
