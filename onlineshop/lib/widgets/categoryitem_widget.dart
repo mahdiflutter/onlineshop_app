5f8ebb57462e8882a106c91cbd8eb4684f133e53
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-02-26 16:54:24 +0330

    Update categoryitem_widget.dart

--- onlineshop/lib/widgets/categoryitem_widget.dart
+++ onlineshop/lib/widgets/categoryitem_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:onlineshop/constants/maincolor_constant.dart';
 import 'package:onlineshop/data/model/category_model.dart';
+import 'package:onlineshop/screens/products_screen.dart';
 import 'package:onlineshop/widgets/cachedimage_widget.dart';
 
 class CateGoryItem extends StatelessWidget {
@@ -11,51 +12,56 @@ class CateGoryItem extends StatelessWidget {
   Widget build(BuildContext context) {
     String categoryColor = 'ff${category!.color}';
     int hexColor = int.parse(categoryColor, radix: 16);
-    return Padding(
-      padding: const EdgeInsets.only(left: 8),
-      child: Column(
-        children: [
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              Container(
-                width: 56,
-                height: 56,
-                decoration: ShapeDecoration(
-                  shadows: [
-                    BoxShadow(
-                      blurRadius: 5,
-                      spreadRadius: -3,
-                      color: Color(hexColor),
-                      offset: const Offset(0, 8),
-                    )
-                  ],
-                  color: Color(hexColor),
-                  shape: ContinuousRectangleBorder(
-                    borderRadius: BorderRadius.circular(40),
+    return InkWell(
+      onTap:(){
+        Navigator.push(context,MaterialPageRoute(builder:(context) => ProductsScreen(),));
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(left: 8),
+        child: Column(
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  width: 56,
+                  height: 56,
+                  decoration: ShapeDecoration(
+                    shadows: [
+                      BoxShadow(
+                        blurRadius: 5,
+                        spreadRadius: -3,
+                        color: Color(hexColor),
+                        offset: const Offset(0, 8),
+                      )
+                    ],
+                    color: Color(hexColor),
+                    shape: ContinuousRectangleBorder(
+                      borderRadius: BorderRadius.circular(40),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                width: 25,
-                height: 25,
-                child: CachedImage(
-                  imageUrl: category!.icon,
+                SizedBox(
+                  width: 25,
+                  height: 25,
+                  child: CachedImage(
+                    imageUrl: category!.icon,
+                  ),
                 ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text(
-            '${category!.title}',
-            style: const TextStyle(
-              fontFamily: 'SB',
-              fontSize: 12,
+              ],
             ),
-          ),
-        ],
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '${category!.title}',
+              style: const TextStyle(
+                fontFamily: 'SB',
+                fontSize: 12,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
