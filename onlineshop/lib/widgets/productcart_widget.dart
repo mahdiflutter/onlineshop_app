25a03bbc6fa60c9a063a982f750ba32240db070a
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-02-26 18:17:33 +0330

    Update productcart_widget.dart

--- onlineshop/lib/widgets/productcart_widget.dart
+++ onlineshop/lib/widgets/productcart_widget.dart
@@ -25,7 +25,7 @@ class ProductCart extends StatelessWidget {
         );
       },
       child: Padding(
-        padding: const EdgeInsets.only(left: 20),
+        padding: const EdgeInsets.only(left: 0),
         child: Container(
           width: 160,
           decoration: BoxDecoration(
