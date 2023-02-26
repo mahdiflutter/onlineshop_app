03ca6c04fca1fabe6838c7cb9271863a274c151d
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-02-26 18:17:28 +0330

    Update products_screen.dart

--- onlineshop/lib/screens/products_screen.dart
+++ onlineshop/lib/screens/products_screen.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:onlineshop/bloc/productbycategory/productbycategory_bloc.dart';
+import 'package:onlineshop/bloc/productbycategory/productbycategory_event.dart';
+import 'package:onlineshop/bloc/productbycategory/productbycategory_state.dart';
 import 'package:onlineshop/constants/maincolor_constant.dart';
+import 'package:onlineshop/widgets/productcart_widget.dart';
 
 class ProductsScreen extends StatefulWidget {
-  const ProductsScreen({super.key});
+  const ProductsScreen(
+      {super.key, required this.categoryId, this.categoryTitle});
+  final String? categoryId;
+  final String? categoryTitle;
 
   @override
   State<ProductsScreen> createState() => _ProductsScreenState();
@@ -10,45 +19,102 @@ class ProductsScreen extends StatefulWidget {
 
 class _ProductsScreenState extends State<ProductsScreen> {
   @override
+  void initState() {
+    BlocProvider.of<ProductByCategoryBloc>(context).add(
+      ProductByCategoryGetProductsEvent(widget.categoryId!),
+    );
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: MainColors.mainBackGround,
       body: SafeArea(
-        child: CustomScrollView(
-          slivers: [
-            //app bar-------
-            const SliverToBoxAdapter(
-              child: CustomAppbar(),
-            ),
-            //none---------
-            const SliverToBoxAdapter(
-              child: SizedBox(height: 20),
-            ),
-            //products list--------
-            SliverPadding(
-              padding: const EdgeInsets.symmetric(horizontal: 44, vertical: 20),
-              sliver: SliverGrid(
-                delegate: SliverChildBuilderDelegate(
-                  (context, index) =>const Text('data'),
+          child: BlocBuilder<ProductByCategoryBloc, ProductByCategoryState>(
+        builder: (context, state) {
+          return CustomScrollView(
+            slivers: [
+              if (state is ProductByCategoryInitState) ...[
+                const SliverToBoxAdapter(
+                  child: Text('data'),
+                )
+              ],
+              if (state is ProductByCategoryLoadingState) ...[
+                const SliverToBoxAdapter(
+                  child: SpinKitRing(
+                    color: MainColors.mainBlue,
+                    size: 30.0,
+                    lineWidth: 3,
+                  ),
+                )
+              ],
+              if (state is ProductByCategoryResponseState) ...[
+                SliverToBoxAdapter(
+                  child: CustomAppbar(title: widget.categoryTitle),
                 ),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 20,
-                  childAspectRatio: 2 / 2.8,
-                ),
+                state.response!.fold((productsException) {
+                  return SliverToBoxAdapter(
+                    child: Text(productsException),
+                  );
+                }, (productsResponse) {
+                  return SliverPadding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 44,
+                      vertical: 20,
+                    ),
+                    sliver: SliverGrid(
+                      delegate: SliverChildBuilderDelegate(
+                        (context, index) => ProductCart(
+                          product: productsResponse[index],
+                        ),
+                        childCount: productsResponse.length,
+                      ),
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        mainAxisSpacing: 10,
+                        childAspectRatio: 2 / 2.8,
+                        crossAxisSpacing: 15,
+                      ),
+                    ),
+                  );
+                })
+              ],
+              /*//app bar-------
+              const SliverToBoxAdapter(
+                child: CustomAppbar(),
               ),
-            )
-          ],
-        ),
-      ),
+              //none---------
+              const SliverToBoxAdapter(
+                child: SizedBox(height: 20),
+              ),
+              //products list--------
+              SliverPadding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 44, vertical: 20),
+                sliver: SliverGrid(
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) => const Text('data'),
+                  ),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 20,
+                    childAspectRatio: 2 / 2.8,
+                  ),
+                ),
+              )*/
+            ],
+          );
+        },
+      )),
     );
   }
 }
 
 class CustomAppbar extends StatelessWidget {
-  const CustomAppbar({
-    super.key,
-  });
+  final String? title;
+  const CustomAppbar({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
@@ -61,30 +127,30 @@ class CustomAppbar extends StatelessWidget {
         ),
         height: 46.0,
         child: Row(
-          children: const [
-            SizedBox(
+          children:  [
+            const SizedBox(
               width: 12,
             ),
-            Icon(
+            const Icon(
               Icons.filter_list_alt,
               color: MainColors.mainBlue,
               size: 30,
             ),
-            Spacer(),
+            const Spacer(),
             Text(
-              'پرفروش ترین ها',
-              style: TextStyle(
+              title!,
+              style: const TextStyle(
                 color: MainColors.mainBlue,
                 fontSize: 17,
               ),
             ),
-            Spacer(),
-            Icon(
+            const Spacer(),
+            const Icon(
               Icons.chevron_right_rounded,
               color: MainColors.mainBlue,
               size: 30,
             ),
-            SizedBox(
+            const SizedBox(
               width: 12,
             ),
           ],
